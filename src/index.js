8904e9019c347deffc09ee7f77f2000dd797e224
Alexandr Bortnikov <alexandr.bortnikoff@gmail.com>
2020-05-19 22:12:04 +0300

    todo app v1

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-  return (
-    <ul>
-      <li>Learn Ract</li>
-      <li>Drink Coffee</li>
-    </ul>
-  )
-}
-
-const AppHeader = () => {
-  return (
-    <h1>My Todo List</h1>
-  )
-}
-
-const SearchPanel = () => {
-  return <input placeholder="search" />
-}
-
-const App = () => {
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
-    </div>
-  )
-}
+import App from './components/app';
 
 ReactDOM.render(<App />,
   document.getElementById('root'));
